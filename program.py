60c7b49ffe31f2f07cfb4bd68e7f11f437f278b0
Superdan <danmo2060@gmail.com>
2019-11-28 15:39:53 +1100

    olk

--- program.py
+++ program.py
@@ -1,20 +1,17 @@
 from phone_inventory import *
-from services.data_service import *
+#from services.data_service import *
 import sys
 
 class InventoryApp(Ui_MainWindow):
     def __init__(self, window):
-        self.setupUi(self,window)
+        self.setupUi(window)
         #direct singal to method of our app class
         #click buttons and inputs here
         #e.g. self.pushbutton.clicked.connect(self.method)
-        
-    def method:
-        pass
     
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 
-ui = FirstApp(MainWindow)
+ui = InventoryApp(MainWindow)
 MainWindow.show()
 app.exec_()
