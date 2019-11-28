bae5829f6e7eb9a77dcae4f6e77cf1d737489dd2
Superdan <danmo2060@gmail.com>
2019-11-29 09:54:08 +1100

    go

--- program.py
+++ program.py
@@ -1,6 +1,15 @@
 from phone_inventory import *
 from services.data_service import *
 import sys
+from mongoengine import *
+
+#connect database with pymongo
+client = pymongo.MongoClient('localhost', 27017)
+db = client.learning_mongo
+#connect database with mongoengine
+connect('learning_mongo')
+
+add_phone(imei="657657",model="iPhone 7",storage="128GB",color="Silver",grade="A")
 
 class InventoryApp(Ui_MainWindow):
     def __init__(self, window):
